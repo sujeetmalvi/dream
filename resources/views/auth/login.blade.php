851b6a7c2b368a06e4a569c893ce86bad1abca3c
Ritesh Singh <1124ritesh@gmail.com>
2016-06-17 23:47:41 +0530

    First commit

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Dream | Log in</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <link href="{{ asset("/bower_components/AdminLTE/bootstrap/css/bootstrap.min.css") }}" rel="stylesheet" type="text/css" />
+    <!-- Font Awesome Icons -->
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <!-- Ionicons -->
+    <link href="http://code.ionicframework.com/ionicons/2.0.0/css/ionicons.min.css" rel="stylesheet" type="text/css" />
+    <!-- Theme style -->
+    <link href="{{ asset("/bower_components/AdminLTE/dist/css/AdminLTE.min.css")}}" rel="stylesheet" type="text/css" />
+    <!-- AdminLTE Skins. We have chosen the skin-blue for this starter
+          page. However, you can choose any other skin. Make sure you
+          apply the skin class to the body tag so the changes take effect.
+    -->
+    <link href="{{ asset("/bower_components/AdminLTE/dist/css/skins/skin-blue.min.css")}}" rel="stylesheet" type="text/css" />
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+</head>
+<body class="hold-transition login-page">
+<div class="login-box">
+  <div class="login-logo">
+    <a href="{{url('/')}}">Dream</a>
+  </div>
+  <!-- /.login-logo -->
+  <div class="login-box-body">
+    <p class="login-box-msg">Sign in to start your session</p>
+
+   <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
+                        {{ csrf_field() }}
+
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}">
+
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                            <label for="password" class="col-md-4 control-label">Password</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control" name="password">
+
+                                @if ($errors->has('password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <div class="checkbox">
+                                    <label>
+                                        <input type="checkbox" name="remember"> Remember Me
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    <i class="fa fa-btn fa-sign-in"></i> Login
+                                </button>
+
+                                <a class="btn btn-link" href="{{ url('/password/reset') }}">Forgot Your Password?</a>
+                            </div>
+                        </div>
+                    </form>
+
+    <!-- /.social-auth-links -->
+
+    <br>
+    
+
+  </div>
+  <!-- /.login-box-body -->
+</div>
+<!-- /.login-box -->
+
+<!-- jQuery 2.1.3 -->
+<script src="{{ asset ("/bower_components/AdminLTE/plugins/jQuery/jQuery-2.2.0.min.js") }}"></script>
+<!-- Bootstrap 3.3.2 JS -->
+<script src="{{ asset ("/bower_components/AdminLTE/bootstrap/js/bootstrap.min.js") }}" type="text/javascript"></script>
+<!-- AdminLTE App -->
+<script src="{{ asset ("/bower_components/AdminLTE/dist/js/app.min.js") }}" type="text/javascript"></script>
+
+</body>
+</html>
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,252 @@
+<!-- Left side column. contains the sidebar -->
+<aside class="main-sidebar">
+
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+
+        <!-- Sidebar user panel (optional) -->
+        <div class="user-panel">
+            <div class="pull-left image">
+                <img src="{{ asset("/bower_components/AdminLTE/dist/img/user2-160x160.jpg") }}" class="img-circle" alt="User Image" />
+            </div>
+            <div class="pull-left info">
+                <p><?=Auth::user()->name;?></p>
+                <!-- Status -->
+                <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+            </div>
+        </div>
+
+        <!-- search form (Optional) -->
+        <form action="#" method="get" class="sidebar-form">
+            <div class="input-group">
+                <input type="text" name="q" class="form-control" placeholder="Search..."/>
+<span class="input-group-btn">
+  <button type='submit' name='search' id='search-btn' class="btn btn-flat"><i class="fa fa-search"></i></button>
+</span>
+            </div>
+        </form>
+        <!-- /.search form -->
+
+        <!-- Sidebar Menu -->
+        <ul class="sidebar-menu">
+            
+            
+            <li class="treeview">
+                <a href="#"><span>Configuration</span> <i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                    <li><a href="#">Country </a></li>
+                    <li><a href="#">State</a></li>
+                    <li><a href="#">District</a></li>
+                    <li><a href="#">Payment Mode</a></li>
+                    <li><a href="#">Student Catagory</a></li>
+                    <li><a href="#">Session</a></li>
+                    <li><a href="#">Class</a></li>
+                    <li><a href="#">Section</a></li>
+                    <li><a href="#">Class Section Relation</a></li>
+                    <li><a href="#">Subject Type</a></li>
+                   
+                    
+                    
+                </ul>
+            </li>
+
+             <li class="treeview">
+                <a href="#"><span>Academic</span> <i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                    
+                    <li><a href="#">Subject</a></li>
+                    <li><a href="#">Class Wise Subject List</a></li>
+                    <li><a href="#">Special Course</a></li>
+                    <li><a href="#">Time Table</a></li>
+                    <li><a href="#">Teacher Wise Time Table</a></li>
+                    <li><a href="#">Assignments</a></li>
+                    <li><a href="#">Certification</a></li>
+                    <li><a href="#">Circular</a></li>
+                </ul>
+            </li>
+
+             <li class="treeview">
+                <a href="#"><span>Student</span> <i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                    <li><a href="#">Student List</a></li>
+                    <li><a href="#">New Admission</a></li>
+                    <li><a href="#">Upgrade Admission</a></li>
+                    <li><a href="#">Student Attendance</a></li>
+                    <li><a href="#">Notification</a></li>
+                </ul>
+            </li>
+
+            <li class="treeview">
+                <a href="#"><span>Examination</span> <i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                    <li><a href="#">CCE</a></li>
+                    <li><a href="#">GPA</a></li>
+                    <li><a href="#">Exam Dates</a></li>
+                    <li><a href="#">Sitting Plan</a></li>
+                    <li><a href="#">Student Result</a></li>
+                    <li><a href="#">Question Papers</a></li>
+
+                </ul>
+            </li>
+
+             <li class="treeview">
+                <a href="#"><span>Finance</span> <i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                    <li><a href="#">Fee Catagory</a></li>
+                    <li><a href="#">Fee Allocation</a></li>
+                    <li><a href="#">Special Fee</a></li>
+                    <li><a href="#">Account Group</a></li>
+                    <li><a href="#">Voucher Master</a></li>
+                    <li><a href="#">Voucher Head</a></li>
+                    <li><a href="#">Create Voucher</a></li>
+                    <li><a href="#">Day Book</a></li>
+                    <li><a href="#">Cash Book</a></li>
+                    <li><a href="#">Bank Book</a></li>
+                   
+                </ul>
+            </li>
+             <li class="treeview">
+                <a href="#"><span>HR and Payroll</span> <i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                    <li><a href="#">Pay Head</a></li>
+                    <li><a href="#">Salary Assign</a></li>
+                    <li><a href="#">Salary Slip</a></li>
+                    <li><a href="#">Generate Salary</a></li>
+                </ul>
+            </li>
+             <li class="treeview">
+                <a href="#"><span>Employee Management</span> <i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                    <li><a href="#">New Employee</a></li>
+                    <li><a href="#">Employee List</a></li>
+                    <li><a href="#">Attendance Performance</a></li>
+                    <li><a href="#">Result Performance</a></li>
+                    <li><a href="#">Attendance</a></li>
+                </ul>
+            </li>
+            <li class="treeview">
+                <a href="#"><span>Leave Management</span> <i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                    <li><a href="#">Leave Catagory</a></li>
+                    <li><a href="#">Leave Details</a></li>
+                    <li><a href="#">Leave Application</a></li>
+                    <li><a href="#">Leave Approvals</a></li>
+
+                </ul>
+            </li>
+
+
+             <li class="treeview">
+                <a href="#"><span>Library</span> <i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                    <li><a href="#">Add Catagory</a></li>
+                    <li><a href="#">Add Books</a></li>
+                    <li><a href="#">Issue Book</a></li>
+                    <li><a href="#">Book Return</a></li>
+                    <li><a href="#">Pending Books</a></li>
+                    
+                   
+                </ul>
+            </li>
+
+             <li class="treeview">
+                <a href="#"><span>Transport</span> <i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                    <li><a href="#">Transport</a></li>
+                    <li><a href="#">Route</a></li>
+                    <li><a href="#">Route Immediate Stations</a></li>
+                    <li><a href="#">Transport Route Allocation</a></li>
+                    <li><a href="#">Assign Driver</a></li>
+                    <li><a href="#">Fuel Record</a></li>
+                    <li><a href="#">Maintenance Record</a></li>
+                    <li><a href="#">Reports</a></li>
+                </ul>
+            </li>
+
+             <li class="treeview">
+                <a href="#"><span>Hostel</span> <i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                    <li><a href="#">Hostel Details</a></li>
+                    <li><a href="#">Hostel Room</a></li>
+                    <li><a href="#">Room Allocation</a></li>
+                    <li><a href="#">Request Details</a></li>
+                    <li><a href="#">Hostel Transfer/Vacate</a></li>
+                    <li><a href="#">Hostel Register</a></li>
+                    <li><a href="#">Visitors</a></li>
+                    <li><a href="#">Performance Report</a></li>
+                    <li><a href="#">Payment Pending Report</a></li>
+                    <li><a href="#">New Payment</a></li>
+                </ul>
+            </li>
+
+             <li class="treeview">
+                <a href="#"><span>Messages</span> <i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                    <li><a href="#">Catagory</a></li>
+                    <li><a href="#">Compose Single Message</a></li>
+                    <li><a href="#">Bulk Message</a></li>
+                    <li><a href="#">Reports</a></li>
+                </ul>
+            </li>
+
+            <li class="treeview">
+                <a href="#"><span>Store Management</span> <i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                    <li><a href="#">Catagory</a></li>
+                    <li><a href="#">New Item</a></li>
+                    <li><a href="#">Sell Item</a></li>
+                    <li><a href="#">Move Item</a></li>
+                    <li><a href="#">Reports</a></li>
+                </ul>
+            </li>
+
+             <li class="treeview">
+                <a href="#"><span>Event Management</span> <i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                    <li><a href="#">New Event</a></li>
+                    <li><a href="#">Upcoming event</a></li>
+                    <li><a href="#">Previous Record</a></li>
+                </ul>
+            </li>
+
+             <li class="treeview">
+                <a href="#"><span>Reports</span> <i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                    <li><a href="#">Student Attendance Report</a></li>
+                    <li><a href="#">Employee Attendance Report</a></li>
+                    <li><a href="#">Financial Reports</a></li>
+                    <li><a href="#">Student Performance Report</a></li>
+                    <li><a href="#">Class Performance Report</a></li>
+                    <li><a href="#">School Performance Report</a></li>
+                    <li><a href="#">Fee Submit Report</a></li>
+                    <li><a href="#">Fee Pending Report</a></li>
+                    <li><a href="#">Admission Report </a></li>
+                    <li><a href="#">Exam Reports</a></li>
+
+                </ul>
+            </li>
+
+             <li class="treeview">
+                <a href="#"><span>Access Rights</span> <i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                    <li><a href="#">Assign Rights</a></li>
+                    <li><a href="#">View Rights</a></li>
+                </ul>
+            </li>
+
+
+             <li class="treeview">
+                <a href="#"><span>Mail</span> <i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                    <li><a href="#">Compose</a></li>
+                    <li><a href="#">Inbox</a></li>
+                    <li><a href="#">Sent</a></li>
+                    <li><a href="#">Draft</a></li>
+                </ul>
+            </li>
+
+             
+        </ul><!-- /.sidebar-menu -->
+    </section>
+    <!-- /.sidebar -->
+</aside>
